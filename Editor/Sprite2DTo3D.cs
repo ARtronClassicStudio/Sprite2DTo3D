c0ab90d48c400d6ee254e3c86b1ce74747a5ff3e
AC Studio <68843488+ARtronClassicStudio@users.noreply.github.com>
2023-03-04 17:49:39 +0300

    Add files via upload

---
+++ Editor/Sprite2DTo3D.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class Sprite2DTo3D : MonoBehaviour
+{
+    [MenuItem("CONTEXT/SpriteRenderer/Convert to 3D")]
+    static void Convert(MenuCommand command)
+    {
+        var sr = (SpriteRenderer)command.context;
+        var m = sr.sharedMaterial;
+        var s = sr.sprite;
+        var t = sr.sprite.texture;
+        var g = sr.gameObject;
+        Mesh mesh = new()
+        {
+            name = s.name,
+            vertices = Array.ConvertAll(s.vertices, x => (Vector3)x),
+            uv = s.uv,
+            triangles = Array.ConvertAll(s.triangles, x => (int)x),
+        };
+        DestroyImmediate(sr);
+        g.AddComponent<MeshFilter>().mesh = mesh;
+        g.AddComponent<MeshRenderer>().sharedMaterial = m;
+        g.GetComponent<MeshRenderer>().sharedMaterial.mainTexture = t;
+        var p = EditorUtility.SaveFilePanelInProject("Save Mesh",mesh.name, "mesh", "asset");
+        if (p != "")
+            AssetDatabase.CreateAsset(mesh, p);
+
+
+    }
+}
